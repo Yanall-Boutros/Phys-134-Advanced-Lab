dce533e0e21ba391adc18519e5fa1192bdd8edb1
Yanall-Boutros <yboutros@ucsc.edu>
2019-01-11 20:28:13 -0800

    finshed adding lins of best fit functions

--- Cavendish/Data/data-1.py
+++ Cavendish/Data/data-1.py
@@ -31,44 +31,30 @@ def chi_squared(ydata, y_bestfit, sigma):
     csr = cs / 18
     return (cs, csr)
 
-def W(n, xdata, ydata, yerror):
-    w_of_n = np.sum(((xdata**n)*ydata)/(yerror**2))
-    return w_of_n
+def linfitfunc(xdata, m=50, b=0):
+    return m*xdata+b
 
-def U(n, xdata, yerror):
-    u_of_n = np.sum((xdata**n)/(yerror**2))
-    return u_of_n
+def LeastSquaresLinFit(xdata, ydata, y_sigma, params, title):
+    # Rewritten from LeastSquareFit provided by Prof. David Smith to be
+    # adapted for a linear fitting function
+   xsmooth = np.linspace(np.min(xdata),np.max(xdata), 1000)
+   fsmooth = linfitfunc(xsmooth, *params)
+   plt.plot(xsmooth, fsmooth, color='green', linewidth=0.5,
+           label='Guess', alpha=0.4)
+   popt, pcov = opt.curve_fit(linfitfunc, xdata, ydata,
+           sigma=y_sigma, p0=params, absolute_sigma=1)
+   fsmooth_next = linfitfunc(xsmooth, *popt)
+   plt.plot(xsmooth, fsmooth_next, color='red', linewidth=0.5,
+            label='Line of Best Fit', alpha=0.6)
+   print("Calculated param vals:")
+   print(popt)
+   plt.legend(loc=1)
+   plt.savefig(title)
 
-def linfit(xdata, ydata, yerror):
-    # output an array of four values in the form
-    # (slope, intercept, sigma_slope, sigma_intercept)
-    # U_n = \sum [x_i^n divided by \sig_i ^ 2]
-    # W_n - \sum [y_i x_i^n divided by \sig_i^2]
-    # D = U_0 * U_2 - U_1\^2
-    U_0 = U(0, xdata, yerror)
-    U_1 = U(1, xdata, yerror)
-    U_2 = U(2, xdata, yerror)
-    W_0 = W(0, xdata, ydata, yerror)
-    W_1 = W(1, xdata, ydata, yerror)
-    
-    D = U_0*U_2 - (U_1**2)
-    slope = ((U_0*W_1) - (U_1*W_0))/D
-    intercept = (U_2*W_0 - U_1*W_1)/D
-    sigma_slope = (U_0/D)**0.5 # sigma_slope might be squared
-    sigma_intercept = (U_2/D)**0.5 # intercept might be squared
-    
-    return np.array([slope, intercept, sigma_slope, sigma_intercept])
-
-def LeastSquaresFit(xdata, ydata, y_sigma):
+def LeastSquaresFit(xdata, ydata, y_sigma, params, title):
    # Least Squares Fit was originally a python program written by Prof.
    # David Smith, I have adapted and altered it to be generalized as an
    # individual function.
-   param_1 = 0.00104 # b val, controls decay
-   param_2 = 60  # a val, controls amp
-   param_3 = 0.0296 # freq
-   param_4 = np.pi/2 - 0.6 # phase
-   param_5 = 101.8 # mean
-   params = np.array([param_1, param_2, param_3, param_4, param_5])
    xsmooth = np.linspace(np.min(xdata),np.max(xdata), 1000)
    fsmooth = damped_osc(xsmooth, *params)
    plt.plot(xsmooth, fsmooth, color='green', linewidth=0.5,
@@ -81,7 +67,7 @@ def LeastSquaresFit(xdata, ydata, y_sigma):
    print("Calculated param vals:")
    print(popt)
    plt.legend(loc=1)
-   plt.savefig("LSF.pdf")
+   plt.savefig(title)
 # ------------------------------------------------------------------------
 # Read in files from directory
 # ------------------------------------------------------------------------
@@ -116,30 +102,72 @@ time_slice_3 = time[2559:4400]
 pot_slice_4 = pot[4400:] # equil 3
 time_slice_4 = time[4400:]
 # ------------------------------------------------------------------------
-# Data Fitting
+# Data Fitting and Plotting
 # ------------------------------------------------------------------------
-# Start with linear fitting on equilibrium points
-equil_1 = linfit(
-            time_slice_0,
-            pot_slice_0,
-            np.ones(len(pot_slice_0))
+# Guess parameters for equilibrium state one
+param_m = 0
+param_b = 50
+params = np.array([param_m, param_b])
+LeastSquaresLinFit(
+        time_slice_0,
+        pot_slice_0,
+        np.ones(len(pot_slice_0)),
+        params,
+        "Equil_1.pdf"
+        )
+# Guess parameters for decay one
+param_1 = 0.00104 # b val, controls decay
+param_2 = 60  # a val, controls amp
+param_3 = 0.0296 # freq
+param_4 = np.pi/2 - 0.6 # phase
+param_5 = 101.8 # mean
+params = np.array([param_1, param_2, param_3, param_4, param_5])
+
+LeastSquaresFit(
+        time_slice_1,
+        pot_slice_1,
+        np.ones(len(pot_slice_1)),
+        params,
+        "Decay 1.pdf"
+        )
+# Guess parameters for equilibrium point two
+param_m = 0
+param_b = 101.8
+params = np.array([param_m, param_b])
+LeastSquaresLinFit(
+        time_slice_2,
+        pot_slice_2,
+        np.ones(len(pot_slice_2)),
+        params,
+        "Equil_2.pdf"
         )
 
-equil_2 = linfit(
-            time_slice_2,
-            pot_slice_2,
-            np.ones(len(pot_slice_2))
-        )
+# Guess parameters for decay two
+param_1 = 0.00104
+param_2 = -3000
+param_3 = 0.0296
+param_4 = -1.022 - 0.5
+param_5 = 53.8
+params = np.array([param_1, param_2, param_3, param_4, param_5])
 
-equil_3 = linfit(
-            time_slice_4,
-            pot_slice_4,
-            np.ones(len(pot_slice_4))
+LeastSquaresFit(
+        time_slice_3,
+        pot_slice_3,
+        np.ones(len(pot_slice_3)),
+        params,
+        "Decay 2.pdf"
         )
-LeastSquaresFit(time_slice_1, pot_slice_1, np.ones(len(pot_slice_1)))
-# ------------------------------------------------------------------------
-# Plot Data
-# ------------------------------------------------------------------------
+# Guess parameters for equilibrium point three
+param_m = 0
+param_b = 53.8
+params = np.array([param_m, param_b])
+LeastSquaresLinFit(
+        time_slice_4,
+        pot_slice_4,
+        np.ones(len(pot_slice_4)),
+        params,
+        "Equil_3.pdf"
+        )
 plt.errorbar(time, pot, yerr=np.ones(len(pot)), capsize=0.5,
         marker='.', markersize=1, alpha=0.3,
         linestyle='', linewidth=0.5, color='blue', label="data")
